e841ebb69e49a3f50bf1516c1f24a05e7cad4cc5
Ali Vazife <124176120+Ali-Vazife@users.noreply.github.com>
2023-06-06 21:45:48 +0330

    Feature/style buttons

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 import { Header } from "./components/Header/header.js";
 import { Footer } from "./components/Footer/footer.js";
-import { MainHomePage } from "./components/MainHomePage/main.js";
 import { Courses } from "./components/Courses/courses.js";
-
 
 window.customElements.define("site-header", Header)
 window.customElements.define("site-footer", Footer)
-window.customElements.define("site-main", MainHomePage)
 window.customElements.define("site-courses", Courses)

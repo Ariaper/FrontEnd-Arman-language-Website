164d2b2ae0d85fa9a5e0eb3fc33f8d889e4d922b
Ali Vazife <124176120+Ali-Vazife@users.noreply.github.com>
2023-06-27 23:27:48 +0330

    Feature/change style of menu bar

--- components/Header/header.js
+++ components/Header/header.js
@@ -29,6 +29,16 @@ crossorigin="anonymous"
       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
      </svg>
     </span></a>
+
+    <div class="shadow rounded menuimgdiv">
+    <img
+      src="/images/Gallery/3.jpg"
+      alt="اردو اردیبهشت"
+      class="menuimg img-fluid"
+    />
+   </div>
+
+
       <a href="index.html" class="mobile-menu-items"><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="list-icon">
       <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
     </svg>
@@ -109,7 +119,7 @@ class Header extends HTMLElement {
     const closeNav = this.shadowRoot.querySelector(".closebtn");
 
     openNav.addEventListener("click", () => {
-      this.shadowRoot.getElementById("mySidepanel").style.width = "25rem";
+      this.shadowRoot.getElementById("mySidepanel").style.width = "36rem";
     })
     closeNav.addEventListener("click", () => {
       this.shadowRoot.getElementById("mySidepanel").style.width = "0";

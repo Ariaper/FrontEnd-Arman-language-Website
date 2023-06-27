de3cd461a3479622d3551f6d02a1e3eec960106f
Ali Vazife <124176120+Ali-Vazife@users.noreply.github.com>
2023-06-27 22:43:21 +0330

    Feature/edit mobile menu

--- components/Header/header.js
+++ components/Header/header.js
@@ -41,7 +41,7 @@ crossorigin="anonymous"
       <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
     </svg>
     </span>برنامه کلاس ها</a>
-      <a href="#" class="mobile-menu-items"><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="list-icon">
+      <a href="loginpage.html" class="mobile-menu-items"><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="list-icon">
       <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
     </svg>
     </span>نمرات</a>
@@ -49,6 +49,10 @@ crossorigin="anonymous"
       <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
     </svg>
     </span>گالری تصاویر</a>
+      <a href="loginpage.html" class="mobile-menu-items"><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="list-icon">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>    
+    </span>ورود کاربران</a>
     </div>
 
     <button class="openbtn align-items-center justify-content-center" ><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="openbtn-svg">
@@ -69,7 +73,7 @@ crossorigin="anonymous"
         <a class="nav-link" href="classProgram.html"> برنامه کلاس ها </a>
       </li>
       <li class="nav-item navbar-li-menu-items">
-        <a class="nav-link" href="#">نمرات</a>
+        <a class="nav-link" href="loginpage.html">نمرات</a>
       </li>
       <li class="nav-item navbar-li-menu-items">
         <a class="nav-link" href="PicGallery.html">گالری تصاویر</a>
